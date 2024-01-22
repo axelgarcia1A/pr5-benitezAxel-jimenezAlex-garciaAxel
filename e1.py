b6269c209bddd0b840d3dc54b6ef24295741f5dd
axelgarcia1A <axel.garcia.7e7@itb.cat>
2024-01-22 11:40:58 +0100

    a

---
+++ e1.py
@@ -0,0 +1,9 @@
+"""
+Nom: Axel Garcia Hernandez, Axel Benitez Parra, Alex Jimenez Navarro
+ASIXc 1A M03 UF1
+Data: 22/01/2024
+Descripció:
+Fer un programa de càlcul de temperatures del mar. Les tasques a fer són:
+Calcular per a l’any  2022: temperatura màxima, mínima i mitjana
+Calcular per període 2000 a 2022: temperatura màxima, mínima i mitjana
+"""
