d48fdab5a0eb4789819d40a9f847099e409a747d
axelgarcia1A <axel.garcia.7e7@itb.cat>
2024-01-22 11:42:34 +0100

    a

--- e1.py
+++ e1.py
@@ -4,6 +4,6 @@ ASIXc 1A M03 UF1
 Data: 22/01/2024
 Descripció:
 Fer un programa de càlcul de temperatures del mar. Les tasques a fer són:
-Calcular per a l’any  2022: temperatura màxima, mínima i mitjana
+Calcular per a l’any 2022: temperatura màxima, mínima i mitjana
 Calcular per període 2000 a 2022: temperatura màxima, mínima i mitjana
 """
