4902cbddddfd17906cd297e820e1ca8aaa32ab3d
axelgarcia1A <axel.garcia.7e7@itb.cat>
2024-01-22 11:41:46 +0100

    a

--- e2.py
+++ e2.py
@@ -3,5 +3,4 @@ Nom: Axel Garcia Hernandez, Axel Benitez Parra, Alex Jimenez Navarro
 ASIXc 1A M03 UF1
 Data: 22/01/2024
 Descripció:
-Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults en català i afegir la traducció en castellà, anglès i klingon
-"""
+Programa que generi una llista de 100 nombres aleatoris entre 1 i 50. Obtenir la mitja dels nombres que es troben a les posicions parelles i la mitja del nombre de les posicions senars. """
